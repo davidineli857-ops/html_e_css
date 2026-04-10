aa08395df6eab6ecab5f98c49ab36d6c7db419d5
davidineli857-ops <davidineli857@gmail.com>
2026-04-10 16:07:46 -0300

    update
    treinando como fazer formulários.

--- ex025/cadastro.js
+++ ex025/cadastro.js
@@ -1,1 +1,12 @@
-document.writte("salve")
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault(); // impede recarregar a página
+
+    let nome = document.getElementById("inome").value;
+    let sobrenome = document.getElementById("isobrenome").value;
+
+    document.body.innerHTML = `
+        <h1>Dados recebidos!</h1>
+        <p>Nome: ${nome}</p>
+        <p>Sobrenome: ${sobrenome}</p>
+    `;
+});
